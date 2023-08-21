346654ae7480e189fb60cbf77597deb28213233e
Nikhil <nikhilshah.net@hotmail.com>
2023-08-21 13:47:46 +0530

    Day 1 - WebAPI Intro

---
+++ Day 1/greetingsAPI/greetingsAPI/Controllers/GreetingsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace greetingsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/greet")]
+        public IActionResult GreetUser()
+        {
+            //webapi methods will return HttpStatus Code and HttpMessage
+            //if everything is ok , send 200 status code - which is OK            
+            return Ok("Hello and Welcome to WebAPI Development");
+        }
+
+        [HttpGet]
+        [Route("/greet/{name}")]
+        public IActionResult GreetUser(string name)
+        {
+            return Ok("Hello " + name);
+        }
+
+        [HttpGet]
+        [Route("/product/list")]
+        public IActionResult Productlist()
+        {
+            List<string> pList = new List<string>()
+            {
+                "Pepsi","Coke","Maggie","Iphone","Dell","Boat","Samsung"
+            };
+            return Ok(pList);
+        }
+
+
+
+    }
+}
